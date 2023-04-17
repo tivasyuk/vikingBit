1d93acdebe10c7b170103e745c06d17819f227cb
tivasyuk <tanyaiv494@gmail.com>
2023-04-17 19:21:37 +0300

    update order api

--- src/utility/api.js
+++ src/utility/api.js
@@ -9,15 +9,15 @@ export const getCurrencyList = async () => {
 }
 
 export const setOrderExchangeData = async (data) => {
-    // const orderResponse = await fetch(`${SERVER_URL}/order`, {
-    //     method: "POST",
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: JSON.stringify(data)
-    // }).then(response => {
-    //     return response.json();
-    // }).then(responseData => {
-    //     return responseData;
-    // })
+    await fetch(`${SERVER_URL}/order`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response => {
+        return response.json();
+    }).then(responseData => {
+        return responseData;
+    })
 }
