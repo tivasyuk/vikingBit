6325838d261cd490b0b0574fe5a632b3059bb792
Maks <unreal9981@gmail.com>
2023-07-04 11:57:53 +0300

    url upd

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,5 +1,5 @@
-const SERVER_URL = 'http://localhost:3003'
-// const SERVER_URL = 'http://139.99.130.52:3003'
+// const SERVER_URL = 'http://localhost:3003'
+const SERVER_URL = 'http://139.99.130.52:3003'
 // const DOMAIN_NAME = 'http://localhost:3000'
 const DOMAIN_NAME = 'http://viking-bit.com'
 
